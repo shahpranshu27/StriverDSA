c3a331c0b4b40c5d64537c87814d476074cd9427
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-28 16:17:45 +0530

    added algorithm

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,5 +1,10 @@
 package Sorting;
 
+// 1. first, we will select the range of unsorted array using loop, that indicates that indicates the starting point of the range. the loop will run from 0 to n-1. the value i=0 means the range is from 0 to n-1
+// 2. now, in each iteration, we will select the minimum element from the range of unsorted array using inner loop
+// 3. after that, we will swap the minimum element with first element, of selected range.
+// 4. finally, after each iteration, we will find that the array is sorted upto first index of the range
+
 public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {13,46,24,52,20,9};
